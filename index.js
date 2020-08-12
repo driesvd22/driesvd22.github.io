0368eb85af00b9ff2928c49fa8bfa63cbb643a09
Dries Van Dievoort <driesvandievoort8@gmail.com>
2020-08-12 11:21:03 +0200

    Navbar Basics JS & JQUERY added

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-(function($) { // Begin jQuery
-    $(function() { // DOM ready
-        // Toggle open and close nav styles on click
-        $('#nav-toggle').click(function() {
-            $('nav ul').slideToggle();
-        }).on('click', function() {
-            this.classList.toggle('active');
-        });
-    }); // end DOM ready
-})(jQuery); // end jQuery
+$(document).ready(function(){
+    $('.toggle').click(function(){
+        $('.menu').toggleClass('active');
+    });
+    document.querySelector( "#nav-toggle" ).addEventListener( "click", function() {
+        this.classList.toggle( "active" );
+    });
+});
