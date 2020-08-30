fa16d6341e70cb0c4e6bd76a8d3cc48389606e10
Dries Van Dievoort <driesvandievoort8@gmail.com>
2020-08-30 15:59:29 +0200

    polishing van al gedane delen

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
 $(document).ready(function(){
     $('.toggle').click(function(){
         $('.menu').toggleClass('active');
+        $('.body').toggleClass('active');
+        $('.html').toggleClass('active');
     });
     document.querySelector( "#nav-toggle" ).addEventListener( "click", function() {
         this.classList.toggle( "active" );
     });
     $('#link-menu1').click(function(){
         $('.menu').toggleClass('active');
+        $('.body').toggleClass('active');
+        $('.html').toggleClass('active');
+    });
+    $('#link-menu2').click(function(){
+        $('.menu').toggleClass('active');
+        $('.body').toggleClass('active');
+        $('.html').toggleClass('active');
+    });
+    $('#link-menu3').click(function(){
+        $('.menu').toggleClass('active');
+        $('.body').toggleClass('active');
+        $('.html').toggleClass('active');
+    });
+    $('#link-menu4').click(function(){
+        $('.menu').toggleClass('active');
+        $('.body').toggleClass('active');
+        $('.html').toggleClass('active');
     });
     document.querySelector( "#link-menu1" ).addEventListener( "click", function() {
         $('#nav-toggle').toggleClass('active')
     });
-    $('#link-menu2').click(function(){
-        $('.menu').toggleClass('active');
-    });
     document.querySelector( "#link-menu2" ).addEventListener( "click", function() {
         $('#nav-toggle').toggleClass('active')
     });
-    $('#link-menu3').click(function(){
-        $('.menu').toggleClass('active');
-    });
     document.querySelector( "#link-menu3" ).addEventListener( "click", function() {
         $('#nav-toggle').toggleClass('active')
     });
-    $('#link-menu4').click(function(){
-        $('.menu').toggleClass('active');
-    });
-    document.querySelector( "#link-menu3" ).addEventListener( "click", function() {
+    document.querySelector( "#link-menu4" ).addEventListener( "click", function() {
         $('#nav-toggle').toggleClass('active')
     });
 });
